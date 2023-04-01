ed478bae014dccd26da7a4f70e7437dcecc8af3c
tjtsai <ttsai@g.hmc.edu>
2023-04-01 06:00:43 +0000

    Modified 01_DataPrep.ipynb by (a) updating the explanatory text to reflect our latest work (as of the ISMIR 2023 paper submission), (b) adding a function verify_audio_dataset() to verify that all required audio files are present, and (c) adding setup_annot_links.sh to automatically create soft links for the piano only annotation files.

---
+++ setup_annot_links.sh
@@ -0,0 +1,4 @@
+ln -s bach5_mov1_O1.beats annot/bach5_mov1_P1.beats
+ln -s beeth1_mov1_O1.beats annot/beeth1_mov1_P1.beats
+ln -s mozart21_mov1_O1.beats annot/mozart21_mov1_P1.beats
+ln -s rach2_mov1_O1.beats annot/rach2_mov1_P1.beats
